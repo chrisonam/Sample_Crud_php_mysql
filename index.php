e1f56e91a1e1578c7fbcd8d59f9debd64b647fbd
chrisonam <chrisonambs@gmail.com>
2023-03-17 01:56:35 -0300

    Adding Tailwind

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <?php
-  include 'db/db_connect.php';    
+  include 'db/db_connect.php'; 
+  include 'assets/header.php';   
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
+   
    <a href=" pages/create_user.php"> Add New User</a>
 
    <table border="1">
